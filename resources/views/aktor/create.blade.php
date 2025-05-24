32e7801bafbf6853a834c768e09c2740507ba827
Ilham Munawar <ilhammunawar55@gmail.com>
2025-05-24 13:57:24 +0700

    Update navbar_dan_sidebar

--- resources/views/aktor/create.blade.php
+++ resources/views/aktor/create.blade.php
@@ -7,42 +7,47 @@
         <div class="my-3 p-3 bg-body rounded shadow-sm">
             <!-- JUDUL -->
             <h2 class="mb-3">Tambah Data User</h2>
-            <a href='{{ url('aktor') }}' class="btn btn-secondary"><< kembali </a>
-            <div class="mb-3 row">
-                <label for="username" class="col-sm-2 col-form-label">Username</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" name="username" id="username" value="" autocomplete="off" readonly>
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="password" class="col-sm-2 col-form-label">Password</label>
-                <div class="col-sm-10 position-relative">
-                    <input type="password" class="form-control" name="password" id="password" value="" autocomplete="new-password" readonly>
-                    <span id="togglePassword" class="position-absolute top-50 end-0 translate-middle-y me-3 cursor-pointer">
-                    </span>
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="email" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" name='email' id="email">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="role" class="col-sm-2 col-form-label">Role</label>
-                <div class="col-sm-10">
-                    <select class="form-control" name="role" id="role">
-                        <option value="">-- Pilih Role --</option>
-                        <option value="supervisor">Supervisor</option>
-                        <option value="staff">Staff</option>
-                        <option value="pekerja">Pekerja</option>
-                    </select>
-                </div>
-            </div>            
-            <div class="mb-3 row">
-                <label for="" class="col-sm-2 col-form-label"></label>
-                <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button></div>
-            </div>
+            <a href='{{ url('aktor') }}' class="btn btn-secondary">
+                << kembali </a>
+                    <div class="mb-3 row">
+                        <label for="username" class="col-sm-2 col-form-label">Username</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="username" id="username" value=""
+                                autocomplete="off" readonly>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="password" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10 position-relative">
+                            <input type="password" class="form-control" name="password" id="password" value=""
+                                autocomplete="new-password" readonly>
+                            <span id="togglePassword"
+                                class="position-absolute top-50 end-0 translate-middle-y me-3 cursor-pointer">
+                            </span>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="email" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name='email' id="email">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="role" class="col-sm-2 col-form-label">Role</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" name="role" id="role">
+                                <option value="">-- Pilih Role --</option>
+                                <option value="supervisor">Supervisor</option>
+                                <option value="staff">Staff</option>
+                                <option value="pekerja">Pekerja</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="" class="col-sm-2 col-form-label"></label>
+                        <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button>
+                        </div>
+                    </div>
         </div>
 
         <!-- Tambahkan FontAwesome untuk ikon -->
@@ -51,7 +56,7 @@
         <!-- Tambahkan FontAwesome untuk ikon -->
         <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
         <script>
-            document.addEventListener("DOMContentLoaded", function () {
+            document.addEventListener("DOMContentLoaded", function() {
                 let username = document.getElementById('username');
                 let password = document.getElementById('password');
 
@@ -67,16 +72,16 @@
             });
 
             // Fitur toggle password
-            document.getElementById('togglePassword').addEventListener('click', function () {
+            document.getElementById('togglePassword').addEventListener('click', function() {
                 let passwordInput = document.getElementById('password');
                 let icon = this.querySelector('i');
 
                 if (passwordInput.type === 'password') {
-                    passwordInput.type = 'text'; 
+                    passwordInput.type = 'text';
                     icon.classList.remove('fa-eye-slash');
                     icon.classList.add('fa-eye');
                 } else {
-                    passwordInput.type = 'password'; 
+                    passwordInput.type = 'password';
                     icon.classList.remove('fa-eye');
                     icon.classList.add('fa-eye-slash');
                 }
@@ -84,7 +89,5 @@
         </script>
 
     </form>
- <!-- AKHIR FORM -->
+    <!-- AKHIR FORM -->
 @endsection
-
-       
--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -63,7 +63,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="#">
+                <a class="nav-link  " href="{{route("pengajuan_proposal.index")}}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
@@ -89,7 +89,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="{{ route('proyek.index') }}">
+                <a class="nav-link  " href="{{ route('proyekdisetujui.index') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 40 40" version="1.1"
@@ -118,7 +118,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="#">
+                <a class="nav-link" href="{{ route('produk.index') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1"
@@ -152,7 +152,7 @@
                 <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="#">
+                <a class="nav-link  " href="{{ route('aktor.index') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1"
