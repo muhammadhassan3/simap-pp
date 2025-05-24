ffc64942f5c4dca12c024ac3de9eb72fb9471d5b
Muhammad Hassan Thalib <73230640+muhammadhassan3@users.noreply.github.com>
2025-05-24 20:17:34 +0700

    Fix Rista UI

--- resources/views/pembelian/detail.blade.php
+++ resources/views/pembelian/detail.blade.php
@@ -1,40 +1,10 @@
-<!DOCTYPE html>
-<html lang="id">
-@php
-    $totalBelanja = 0;
-    foreach ($detailPembelian as $item) {
-        $totalBelanja += $item->qty * $item->harga_satuan;
-    }
-@endphp
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail Pembelian</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <style>
-        body {
-            background-color: #f8f9fa;
+<x-layout>
+    @php
+        $totalBelanja = 0;
+        foreach ($detailPembelian as $item) {
+            $totalBelanja += $item->qty * $item->harga_satuan;
         }
-
-        .card {
-            border-radius: 10px;
-            margin-bottom: 20px;
-        }
-
-        .section-title {
-            font-weight: bold;
-            padding: 10px;
-            background-color: #e9ecef;
-            border-radius: 8px;
-        }
-    </style>
-</head>
-
-<body>
+    @endphp
     <div class="container mt-4">
         <!-- Bagian Pembelian -->
         <a href="{{ route('pembelian.tampil') }}" class="text-dark">
@@ -48,7 +18,7 @@
                 <p><strong>Proyek:</strong>
                     {{ $pembelian->proyekDisetujui->pengajuanProposal->nama_proyek ?? 'Tidak Ditemukan' }}</p>
                 <p><strong>Nota:</strong> <a href="{{ asset('storage/' . $pembelian->foto_nota) }}"
-                        target="_blank">Lihat
+                                             target="_blank">Lihat
                         Nota</a></p>
                 <p><strong>Total: Rp. {{ number_format($totalBelanja, 0, ',', '.') }}</strong>
             </div>
@@ -94,7 +64,7 @@
                             </div>
                             <div class="form-group me-2">
                                 <input type="text" class="form-control"
-                                    value="{{ number_format($detail->harga_satuan, 0, ',', '.') }}" readonly>
+                                       value="{{ number_format($detail->harga_satuan, 0, ',', '.') }}" readonly>
                             </div>
                             <div class="form-group me-2">
                                 <input type="text" class="form-control" value="{{ $detail->total_harga }}" readonly>
@@ -104,13 +74,11 @@
                                     <i class="fa-solid fa-pen-to-square"></i> Edit
                                 </a>
                             </div>
-                            
+
                         </div>
                     @endforeach
                 </div>
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</x-layout>
