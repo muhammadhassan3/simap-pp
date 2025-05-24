e2a9adc7fd6e76d70e1c5419d618622a1f09282c
Ilham Munawar <ilhammunawar55@gmail.com>
2025-05-24 14:14:40 +0700

    Update dashboard

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,6 +1,4 @@
 <x-layout>
-    @section('content')
-        <h2></h2>
-        <p>Selamat datang {{$user->username}}.</p>
-    @endsection
+    <h2></h2>
+    <p>Selamat datang {{ $user->username }}.</p>
 </x-layout>
