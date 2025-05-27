98522c07ad4589687489f079989c707b03d50483
Ilham Munawar <ilhammunawar55@gmail.com>
2025-05-27 15:46:09 +0700

    update

--- resources/views/sewa_alat/index.blade.php
+++ resources/views/sewa_alat/index.blade.php
@@ -141,64 +141,61 @@
         <div class="top-bar">
             <a href="/sewa_alat/create" class="add-button">+ Tambah Alat Berat</a>
             <input type="text" id="searchInput" class="search-box" placeholder="Cari..." onkeyup="searchTable()">
-
-                <div class="table-responsive">
-                    <table class="table table-hover align-middle border rounded shadow-sm">
-                        <thead class="table-secondary">
-                            <tr>
-                                <th>ID</th>
-                                <th>Nama Alat</th>
-                                <th>Harga Sewa</th>
-                                <th>Nama Perusahaan</th>
-                                <th>Durasi</th>
-                                <th>Qty</th>
-                                <th>Nama Proyek</th>
-                                <th>Detail</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($sewaAlat as $alat)
-                                <tr>
-                                    <td>{{ $alat->id }}</td>
-                                    <td>{{ $alat->nama_alat }}</td>
-                                    <td>Rp. {{ number_format($alat->harga_sewa, 0, ',', '.') }}/jam</td>
-                                    <td>{{ $alat->customer->nama_customer }}</td>
-                                    <td>{{ $alat->durasi }} menit</td>
-                                    <td>{{ $alat->qty }}</td>
-                                    <td>{{ $alat->tempatProyek ? $alat->tempatProyek->nama_tempat : 'Tidak Diketahui' }}
-                                    </td>
-                                    <td>{{ $alat->detail }}</td>
-                                    <td class="action-buttons">
-                                        <a href="/sewa_alat/{{ $alat->id }}/edit" class="edit-button text-black">
-                                            <i class="fas fa-pencil-alt"></i>
-                                        </a>
-                                        <form action="/sewa_alat/{{ $alat->id }}" method="POST"
-                                            style="display: inline;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="delete-button" onclick="confirmDelete(event)">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                    <path
-                                                        d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z">
-                                                    </path>
-                                                </svg>
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            @if ($sewaAlat->isEmpty())
-                                <tr>
-                                    <td colspan="9" style="text-align: center;">Tidak ada data tersedia.</td>
-                                </tr>
-                            @endif
-                        </tbody>
-                    </table>
-
-                </div>
-            </div>
         </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nama Alat</th>
+                    <th>Harga Sewa</th>
+                    <th>Nama Perusahaan</th>
+                    <th>Durasi</th>
+                    <th>Qty</th>
+                    <th>Nama Proyek</th>
+                    <th>Detail</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($sewaAlat as $alat)
+                    <tr>
+                        <td>{{ $alat->id }}</td>
+                        <td>{{ $alat->nama_alat }}</td>
+                        <td>Rp. {{ number_format($alat->harga_sewa, 0, ',', '.') }}/jam</td>
+                        <td>{{ $alat->customer->nama_customer }}</td>
+                        <td>{{ $alat->durasi }} menit</td>
+                        <td>{{ $alat->qty }}</td>
+                        <td>{{ $alat->tempatProyek ? $alat->tempatProyek->nama_tempat : 'Tidak Diketahui' }}</td>
+                        <td>{{ $alat->detail }}</td>
+                        <td class="action-buttons">
+                            <a href="/sewa_alat/{{ $alat->id }}/edit" class="edit-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path
+                                        d="M14.1 4.22l5.66 5.66-9.19 9.19-5.66-5.66 9.19-9.19M15.5 3c-.4 0-.8.15-1.06.44L3.44 14.5a1.5 1.5 0 000 2.12l4.95 4.95c.29.29.67.44 1.06.44s.78-.15 1.06-.44L20.56 9.5a1.5 1.5 0 000-2.12l-4.95-4.95A1.5 1.5 0 0015.5 3z">
+                                    </path>
+                                </svg>
+                            </a>
+                            <form action="/sewa_alat/{{ $alat->id }}" method="POST" style="display: inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="delete-button" onclick="confirmDelete(event)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path
+                                            d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z">
+                                        </path>
+                                    </svg>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                @if ($sewaAlat->isEmpty())
+                    <tr>
+                        <td colspan="9" style="text-align: center;">Tidak ada data tersedia.</td>
+                    </tr>
+                @endif
+            </tbody>
+        </table>
     </div>
 
 </x-layout>
